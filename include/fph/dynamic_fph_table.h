0f98c72cff371c43ac430298a61463491350a605
renzibei <31380016+renzibei@users.noreply.github.com>
2026-08-10 23:39:18 +0800

    Add Apache-2.0 license and third-party notices
    LICENSE, NOTICE (robin-hood-hashing MIT and Abseil attributions), and
    per-file SPDX headers.
    
    (cherry picked from commit b550b93f153d46e7b24c3462c7f810b723ce157a)

--- include/fph/dynamic_fph_table.h
+++ include/fph/dynamic_fph_table.h
@@ -1,4 +1,23 @@
 // Dynamic Flash Perfect Hash Table
+//
+// Copyright 2022-2026 renzibei
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// This file contains portions derived from third-party projects; see the
+// NOTICE file and the inline attributions below.
 
 /**
  * This file provides dynamic perfect hash set and map, which are ultra-fast in query but slow in
@@ -381,7 +400,30 @@ namespace fph {
 
     namespace dynamic::detail {
 
-        // from robin_hood hash
+        // The UnalignedLoad() and HashBytes() functions below are derived from
+        // robin-hood-hashing (https://github.com/martinus/robin-hood-hashing),
+        // used under the MIT License:
+        //
+        //   Copyright (c) 2018-2021 Martin Ankerl
+        //
+        //   Permission is hereby granted, free of charge, to any person obtaining a
+        //   copy of this software and associated documentation files (the
+        //   "Software"), to deal in the Software without restriction, including
+        //   without limitation the rights to use, copy, modify, merge, publish,
+        //   distribute, sublicense, and/or sell copies of the Software, and to
+        //   permit persons to whom the Software is furnished to do so, subject to
+        //   the following conditions:
+        //
+        //   The above copyright notice and this permission notice shall be included
+        //   in all copies or substantial portions of the Software.
+        //
+        //   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+        //   OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+        //   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+        //   IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+        //   CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+        //   TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+        //   SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
         template <typename T>
         inline T UnalignedLoad(void const* ptr) noexcept {
             // using memcpy so we don't get into unaligned load problems.
